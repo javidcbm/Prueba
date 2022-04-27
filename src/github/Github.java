85f1dfc7d52ac2e3ab78f1531dd094bb8d9bde86
dirgo <dirgo@MSI.home>
2022-04-27 02:17:00 +0200

    Commit2

--- src/github/Github.java
+++ src/github/Github.java
@@ -14,7 +14,7 @@ public class Github {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola qué tal");
     }
     
 }
